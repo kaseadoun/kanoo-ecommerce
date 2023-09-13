9644073447819b6581aeb79bd006ef313717b0f9
Karsten <karstenkleung@gmail.com>
2023-09-13 14:11:56 -0400

    Create new custom route for Brief

--- Program.cs
+++ Program.cs
@@ -24,4 +24,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "Brief",
+    pattern: "brief",
+    defaults: new {controller="Home", Action="Brief"}
+);
+
 app.Run();
