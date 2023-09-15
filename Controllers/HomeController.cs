b0db31c67e5373231866c9d9d13845087566ef8e
Karsten Leung <karstenkleung@gmail.com>
2023-09-15 11:19:46 -0400

    Code in view to iterate through the data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,7 +25,24 @@ public class HomeController : Controller
 
     public IActionResult Brief()
     {
-        return View();
+        List<Dictionary<string, string>> briefInfo = new List<Dictionary<string, string>>
+        {
+            // Dictionary for company info
+            new Dictionary<string, string> {
+                {"Company Name", "Kanoo"},
+                {"Company Description", "Kanoo has a focus of providing a relaxing way of setting up vacations by providing packages and planning services."},
+                {"Group Members and Roles", "Karsten Leung - Full Stack Developer<br/>Emily Rose - Full Stack Developer<br/>John Gu - Full Stack Developer"}
+            },
+            // Dictionary for proposal
+            new Dictionary<string, string> {
+                {"Description of the Site", "Kanoo is an eCommerce business that provides vacation packages and planning services."},
+                {"The Products or Services", "Vacation packages and planning services."},
+                {"Industry Needs to be Fulfilled", "Travel and planning needs."},
+                {"Roles User Need to Fill", "Customer role."}
+            }
+        };
+
+        return View(briefInfo);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
